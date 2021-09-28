b110a732b1b31e0cb47eb35f66c2f84f174bd274
김은휼 <valhalla.host@gmail.com>
2021-09-28 20:25:48 +0900

    IntroPage 컴포넌트 구현(하위 컴포넌트 제외)

---
+++ src/pages/IntroPage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Section from 'components/Section';
+import Logo from 'components/Logo';
+import MetricsContainer from 'components/MetricsContainer';
+import AwardsContainer from 'components/AwardsContainer';
+import './style.scss';
+
+export const BLOCK = 'statistic-section';
+const LOGO_URL = '/intro/images/triple@2x.png';
+
+export default function IntroPage() {
+	return (
+		<div className="intro-page">
+			<Section className={BLOCK}>
+				<Logo className={`${BLOCK}--content-logo`} url={LOGO_URL}>
+					2019년 2월 기준
+				</Logo>
+				<MetricsContainer></MetricsContainer>
+				<AwardsContainer></AwardsContainer>
+			</Section>
+			;
+		</div>
+	);
+}
